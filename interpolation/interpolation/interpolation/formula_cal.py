a1131f6e87cf28ad3980a5bc458b8fbbedf6b4dd
hejun <1124418652@qq.com>
2018-09-12 16:21:30 +0800

    add function cal_rotate_img

--- interpolation/interpolation/interpolation/formula_cal.py
+++ interpolation/interpolation/interpolation/formula_cal.py
@@ -2,15 +2,35 @@ import time
 import numpy as np 
 from sympy import *
 
+def cal_rotate_formula():
+	start = time.time()
+
+	res_x, res_y, dest_x, dest_y, gain, angle = \
+		symbols("res_x, res_y, dest_x, dest_y, gain, angle")
+
+	rotate_mat = np.mat([[cos(angle) * gain, -sin(angle) * gain, 0],
+						[sin(angle) * gain, cos(angle) * gain, 0],
+						[0, 0, 1]])
+
+	res_coord = np.mat([res_x, res_y, 1]) * rotate_mat
+	end = time.time()
+	
+	print("calculate the coordinate system of rotation manupulate: ")
+	print("dest_x: %s\ndest_y: %s" %(res_coord[0, 0], res_coord[0, 1]))
+	print("Time used: %s\n" %(end - start))
+
 def inverse_rotate_formula():
 	# (res_x * cos(angle) + res_y * sin(angle)) * gain - dest_x
 	# (res_y * cos(angle) - res_x * sin(angle)) * gain - dest_y
 
 	start = time.time()
-	res_x, res_y, dest_x, dest_y, gain, angle = symbols("res_x, res_y, dest_x, dest_y, gain, angle")
-	res = solve([(res_x * cos(angle) + res_y * sin(angle)) * gain - dest_x, (res_y * cos(angle) - res_x * sin(angle)) * gain - dest_y], \
-			[res_x, res_y])
-	print("the formula of rotation in 2D:")
+	res_x, res_y, dest_x, dest_y, gain, angle = \
+		symbols("res_x, res_y, dest_x, dest_y, gain, angle")
+	
+	res = solve([(res_x * cos(angle) + res_y * sin(angle)) * gain - dest_x, \
+		(res_y * cos(angle) - res_x * sin(angle)) * gain - dest_y], [res_x, res_y])
+	
+	print("the formula of inverse rotation in 2D:")
 	print("res_x: %s\nres_y: %s" %(res[res_x], res[res_y]))
 	end = time.time()
 	print("Time used: %s\n" %(end - start))
@@ -18,8 +38,11 @@ def inverse_rotate_formula():
 def cal_img_rotate():
 	start  = time.time()
 
-	res_width, res_height, dest_width, dest_height = symbols("res_width, res_height, dest_width, dest_height")
-	angle, res_x, res_y, dest_x, dest_y = symbols("angle, res_x, res_y, dest_x, dest_y")
+	res_width, res_height, dest_width, dest_height = \
+		symbols("res_width, res_height, dest_width, dest_height")
+	angle, res_x, res_y, dest_x, dest_y = \
+		symbols("angle, res_x, res_y, dest_x, dest_y")
+	gain = symbols("gain")
 
 	img2math = np.mat([[1, 0, 0],
 						[0, -1, 0],
@@ -46,8 +69,9 @@ def cal_img_rotate():
 def inverse_img_rotate():
 	start = time.time()
 
-	res_width, res_height, dest_width, dest_height = symbols("res_width, res_height, dest_width, dest_height")
-	angle = symbols("angle")
+	res_width, res_height, dest_width, dest_height = \
+		symbols("res_width, res_height, dest_width, dest_height")
+	angle, gain = symbols("angle, gain")
 	res_x, res_y, dest_x, dest_y = symbols("res_x, res_y, dest_x, dest_y")
 
 	img2math = np.mat([[1, 0, 0],
@@ -72,6 +96,7 @@ def inverse_img_rotate():
 	print("Time used: %s\n" %(end - start))
 
 def main():
+	cal_rotate_formula()
 	inverse_rotate_formula()
 	cal_img_rotate()
 	inverse_img_rotate()
