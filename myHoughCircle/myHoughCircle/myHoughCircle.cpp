47be089a9719e1b8c5a2b52f41852705daeb68df
hejun <1124418652@qq.com>
2018-12-06 22:02:29 +0800

    create new project myHoughCircle

--- myHoughCircle/myHoughCircle/myHoughCircle.cpp
+++ myHoughCircle/myHoughCircle/myHoughCircle.cpp
@@ -5,136 +5,171 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
-static void
-icvHoughCirclesGradient(CvMat* img, float dp, float min_dist,
-						int min_radius, int max_radius,
-						int canny_threshold, int acc_threshold,
-						CvSeq* circles, int circles_max);
+#define PCB_MODE 1
+#define NORMAL_MODE 0
+
+void myHoughCircle(const cv::Mat &src_img, 
+				   std::vector<cv::Vec3f> &circles, float dp, 
+				   float min_dist, int min_radius, 
+				   int max_radius, int canny_threshold, 
+				   int accum_threshold, int mode = PCB_MODE);
+
+void myPointSorted(std::vector<std::vector<int>> &centers, int begin, int end);
+/** @根据centers[][2]中的值对centers进行快速排序，排序方式为倒序
+
+@note
+
+-   排序结果直接替换掉centers中原先的值
+
+@param centers: 需要进行排序的向量
+@param begin: centers向量的起始位置
+@param end: centers向量中的终止位置
+*/
+int Partition(std::vector<std::vector<int>> &centers, int begin, int end);
 
 int main()
 {
+	std::string img_path = "img/timg1.jpg";
+	cv::Mat src_img = cv::imread(img_path, 0);
+	if (src_img.empty())
+		exit(1);
+	std::vector<cv::Vec3f> circles;
+	myHoughCircle(src_img, circles, 1, 2, 0, 30, 100, 100);
+	cv::imshow("src_img", src_img);
+	cv::waitKey(0);
     return 0;
 }
 
-static void
-icvHoughCirclesGradient(CvMat* img, float dp, float min_dist,
-	int min_radius, int max_radius,
-	int canny_threshold, int acc_threshold,
-	CvSeq* circles, int circles_max)
+void myHoughCircle(const cv::Mat &src_img,
+	std::vector<cv::Vec3f> &circles, float dp,
+	float min_dist, int min_radius,
+	int max_radius, int canny_threshold,
+	int accum_threshold, int mode)
 {
-	const int SHIFT = 10, ONE = 1 << SHIFT, R_THRESH = 30;
-	cv::Ptr<CvMat> dx, dy;
-	cv::Ptr<CvMat> edges, accum, dist_buf;
-	std::vector<int> sort_buf;
-	cv::Ptr<CvMemStorage> storage;
+	const int SHIFT = 10, ONE = 1 << SHIFT;
+	cv::Mat dx, dy;          // 保存x，y方向的梯度信息
+	cv::Mat gradient;
+	std::vector<std::vector<int>> centers;     // 用于保存圆心的坐标
+	std::vector<cv::Point> edge_points;
+	cv::Mat accum;           // 累加器图像
+	int src_rows = src_img.rows;
+	int src_cols = src_img.cols;
+	dp = dp >= 1.f ? dp : 1.f;
+	float idp = 1 / dp;
+	
+	/* 初始化dx，dy，gradient以及accum图像 */
+	dx.create(cv::Size(src_cols, src_rows), CV_16SC1);
+	dy.create(cv::Size(src_cols, src_rows), CV_16SC1);
+	gradient.create(cv::Size(src_cols, src_rows), CV_32FC1);
+	accum = cv::Mat::zeros(cv::Size(ceil(src_cols * idp), ceil(src_rows * idp)), CV_32SC1);
 
-	int x, y, i, j, k, center_count, nz_count;
-	float min_radius2 = (float)min_radius * min_radius;
-	float max_radius2 = (float)max_radius * max_radius;
-	int rows, cols;     // 边缘图像的行数和列数
-	int arows, acols;   // 累加器图像的行数和列数
-	int astep, *adata;  // astep为累加器每行的大小，adata指向累加器数据域的首地址
-	float* ddata;       // 即dist_data，距离数据
-	CvSeq *nz, *centers;     // centers 存放候选中心的位置
-	float idp, dr;      // idp 为 dp 的倒数
-	CvSeqReader reader; // 顺序读取序列中的每个值
+	cv::Scharr(src_img, dx, dx.depth(), 1, 0);
+	cv::Scharr(src_img, dy, dy.depth(), 0, 1);
 
-	edges = cvCreateMat(img->rows, img->cols, CV_8UC1); // 边缘图像
-	cvCanny(img, edges, MAX(canny_threshold / 2, 1), canny_threshold, 3);
-	dx = cvCreateMat(img->rows, img->cols, CV_16SC1);   // 16位单通道图像，用来存储二值边缘图像x方向的一阶导数  
-	dy = cvCreateMat(img->rows, img->cols, CV_16SC1);   // 16位单通道图像，用来存储二值边缘图像y方向的一阶导数
-	cvSobel(img, dx, 1, 0, 3);
-	cvSobel(img, dy, 0, 1, 3);
-
-	if (dp < 1.f)
-		dp = 1.f;
-	idp = 1.f / dp;
-
-	// 创建累积平面，大小为原图像的idp倍，32位单通道
-	accum = cvCreateMat(cvCeil(img->rows * idp) + 2, cvCeil(img->cols * idp) + 2, CV_32SC1);
-	cvZero(accum);     //初始化为0
-
-	storage = cvCreateMemStorage();     // 创建内存存储器，使用默认参数0，默认大小为64KB
-	nz = cvCreateSeq(CV_32SC2, sizeof(CvSeq), sizeof(CvPoint), storage);     // 用来存储非0点
-	centers = cvCreateSeq(CV_32SC1, sizeof(CvSeq), sizeof(int), storage);
-
-	rows = img->rows;
-	cols = img->cols;
-	arows = accum->rows - 2;
-	acols = accum->cols - 2;
-	adata = accum->data.i;              // cvMat对象的union对象的i成员
-
-	astep = accum->step / sizeof(adata[0]);
-
-	for (y = 0; y < rows; y++)
+	/* 更新accum平面 */
+	for (int y = 0; y < src_rows; ++y) 
 	{
-		const uchar* edges_row = edges->data.ptr + y * edges->step;          // 使用指针访问数据，指向边界图像中的每一行的行首地址
-		const short* dx_row = (const short*)(dx->data.ptr + y * dx->step);
-		const short* dy_row = (const short*)(dy->data.ptr + y * dy->step);
-
-		for (x = 0; x < cols; x++)
+		const short* dx_row = static_cast<const short*>(dx.ptr<short>(y));
+		const short* dy_row = static_cast<const short*>(dy.ptr<short>(y));
+		if (y == int(src_rows / 4))
+			y += int(src_rows / 2);
+		for (int x = 0; x < src_cols; ++x)
 		{
-			float vx, vy;       // vx,vy分别为x,方向的梯度值
-			int sx, sy, x0, y0, x1, y1, r, k;
-			CvPoint pt;
-
-			vx = dx_row[x];     // x梯度中每一行的像素值
-			vy = dy_row[x];     // y梯度中每一行的像素值
-
-			if (!edges_row[x] || (vx == 0 && vy == 0))  // 如果不是canny边缘，跳过该点
+			if (x == int(src_cols / 4))
+				x += int(src_cols / 2);
+			int vx = dx_row[x];      // 每一点对应的梯度值
+			int vy = dy_row[x];
+			if (!vx && !vy)
 				continue;
+			cv::Point pt;
+			float sx, sy;            // 记录归一化之后的梯度值
+			int x0, y0;              // src_img 中的点在 accum 图像中对应的位置
+			float x1, y1;              // 记录 accum 图像中最小半径到最大半径之间的法线点
+			float mag = sqrt(vx * vx + vy * vy);
+			sx = (float)vx / mag;    // 将x，y方向的梯度值进行归一化
+			sy = (float)vy / mag;
+			x0 = round(x * idp);     // 将src_img中的点对应到accum中
+			y0 = round(y * idp);
 
-			float mag = sqrt(vx * vx + vy * vy);        // 求解该点的梯度值
-			assert(mag >= 1);
-			sx = cvRound((vx * idp) * ONE / mag);
-			sy = cvRound((vy * idp) * ONE / mag);
-
-			x0 = cvRound((x * idp) * ONE);
-			y0 = cvRound((y * idp) * ONE);
-
-			for (k = 0; k < 2; k++)    // 对累加器进行累加，k=0为梯度方向，k=1为梯度反方向 
+			for (int k = 0; k < 2; ++k)
 			{
-				x1 = x0 + min_radius * sx;
-				y1 = y0 + min_radius * sy;
-
-				for (r = min_radius; r < max_radius; x1 += sx, y1 += sy, r++)
+				x1 = x0 + min_radius * sx;   // 法线方向 x 的起始坐标
+				y1 = y0 + min_radius * sy;   // 法线方向 y 的起始坐标
+				for (int r = min_radius; r <= max_radius; ++r)
 				{
-					int x2 = x1 >> SHIFT, y2 = y1 >> SHIFT;     // 变回真实的坐标
-					if ((unsigned)x2 >= (unsigned)acols ||
-						(unsigned)y2 >= (unsigned)arows)
+					if (x1 >= accum.cols - 1 || y1 >= accum.rows - 1 ||
+						x1 <= 0 || y1 <= 0)
 						break;
-					adata[y2 * astep + x2]++;
+					accum.ptr<short>((int)round(y1))[(int)round(x1)]++;
+					//std::cout << accum.ptr<short>(y1)[x1] << std::endl;
+					x1 = x1 + sx;
+					y1 = y1 + sy;
 				}
 				sx = -sx; sy = -sy;
 			}
 			pt.x = x; pt.y = y;
-			cvSeqPush(nz, &pt);        // 把边缘不为0并且梯度不为0的点压入堆栈
+			edge_points.push_back(pt);       // 记录梯度不为0的点
 		}
 	}
-
-	nz_count = nz->total;
-	if (!nz_count)
+	int num_edge_points = edge_points.size();
+	if (!num_edge_points)
 		return;
 
-	for (y = 1; y < arows - 1; y++)
+	/* 统计accum平面中符合要求的圆心坐标 */
+	for (int y = 1; y < accum.rows - 1; ++y)
 	{
-		for (x = 1; x < acols - 1; x++)
+		const short* accum_row_before = static_cast<const short*>(accum.ptr<short>(y - 1));
+		const short* accum_row = static_cast<const short*>(accum.ptr<short>(y));
+		const short* accum_row_after = static_cast<const short*>(accum.ptr<short>(y + 1));
+		for (int x = 1; x < accum.cols - 1; ++x)
 		{
-			int base = y * (acols + 2) + x;
-			if (adata[base] > acc_threshold &&
-				adata[base] > adata[base - 1] &&
-				adata[base] > adata[base + 1] &&
-				adata[base] > adata[base - acols - 2] &&
-				adata[base] > adata[base + acols + 2])
-				cvSeqPush(centers, &base);
+			if (accum_row[x] > accum_threshold && accum_row[x] > accum_row[x + 1] &&
+				//accum_row[x] > accum_row[x - 1] && accum_row[x] > accum_row_before[x] &&
+				accum_row[x] > accum_row_after[x])
+			{
+				std::vector<int> center_candidate(3);
+				center_candidate[0] = x;
+				center_candidate[1] = y;
+				center_candidate[2] = (int)accum_row[x];
+				centers.push_back(center_candidate);
+			}
 		}
 	}
-
-	center_count = centers->total;
-	if (!center_count)
+	if (!centers.size())
 		return;
+	myPointSorted(centers, 0, centers.size() - 1);       // 对圆心坐标根据交点累积数进行排序
+	cv::Mat tmp = src_img.clone();
+	for (int i = 0; i < centers.size(); ++i)
+	{
+		tmp.at<uchar>(round(centers[i][1] * dp), round(centers[i][0] * dp)) = 0;
+	}
+	cv::imshow("tmp", tmp);
+	cv::waitKey(0);
+}
 
-	sort_buf.resize(MAX(center_count, nz_count));
-	cvCvtSeqToArray(centers, &sort_buf[0]);        // 把序列转换成数组，即把序列centers中的数据放入到sort_buf的容器中
+void myPointSorted(std::vector<std::vector<int>> &centers, int begin, int end)
+{
+	if (begin < end) 
+	{
+		int pivotloc = Partition(centers, begin, end);
+		myPointSorted(centers, begin, pivotloc - 1);
+		myPointSorted(centers, pivotloc + 1, end);
+	}
+}
 
+int Partition(std::vector<std::vector<int>> &centers, int begin, int end)
+{
+	int pivotkey = centers[begin][2];
+	std::vector<int> tmp = centers[begin];
+	while (begin < end)
+	{
+		while (begin < end && centers[end][2] <= pivotkey)
+			--end;
+		centers[begin] = centers[end];
+		while (begin < end && centers[begin][2] >= pivotkey)
+			++begin;
+		centers[end] = centers[begin];
+	}  // break when end == begin
+	centers[begin] = tmp;
+	return begin;
 }
